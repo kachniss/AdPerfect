4feaf1560791db8fd5ced2b7dc98285253c73829
Kachniss <vopalkova.katerina@gmail.com>
2018-09-30 17:19:57 -0700

    Edit fadeOut

--- RememberingObit_files/status-bar.js
+++ RememberingObit_files/status-bar.js
@@ -1,7 +1,7 @@
 $(function(){
 
     $(".js-close-status-bar").on('click', function() {
-       $(".story-status-top-bar").fadeOut();
+       $(".story-status-top-bar").fadeOut(1000);
        $(".story-panel").toggleClass("story-panel-dimmed");
     });
 
