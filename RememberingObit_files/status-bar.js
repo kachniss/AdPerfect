661a6bf2688c10ecdf6b96b317f2cb213627c4ee
Kachniss <vopalkova.katerina@gmail.com>
2018-09-30 17:35:21 -0700

    Fix font weight

--- RememberingObit_files/status-bar.js
+++ RememberingObit_files/status-bar.js
@@ -1,12 +1,6 @@
 $(function(){
-
     $(".js-close-status-bar").on('click', function() {
        $(".story-status-top-bar").fadeOut(1000);
        $(".story-panel").toggleClass("story-panel-dimmed");
     });
-
 });
-
-
-
-
